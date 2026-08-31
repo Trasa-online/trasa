5faa3994fe8d0ba0d172eb226edcd61cc68bcc38
nyszje <nat.maz98@gmail.com>
2026-08-31 15:01:10 +0200

    fix(admin): wyrejestruj rogue service-worker + wyczysc cache przy wejsciu
    Objaw: panel serwowal STARY build (stara wersja logowania) mimo poprawnego deployu
    (live bundle byl nowy, brak SW w configu). Przyczyna: SW zarejestrowany wczesniej na
    admin.trasa.travel (gdy domena serwowala apke z PWA) serwowal cache. main.tsx teraz
    wyrejestrowuje wszystkie SW + czysci Cache Storage przy kazdym wejsciu (samonaprawa).

--- src/admin/main.tsx
+++ src/admin/main.tsx
@@ -8,6 +8,19 @@ import AdminApp from "./AdminApp";
 import "@/index.css";
 import "./admin.css";
 
+// Panel admina NIE ma i nie powinien miec service-workera. Jesli jakis SW zostal
+// wczesniej zarejestrowany na tej domenie (np. gdy chwilowo serwowala apke z PWA),
+// przegladarka moze serwowac STARY build mimo deployu (stad "stara" wersja logowania).
+// Wyrejestrowujemy wszystkie SW i czyscimy cache przy kazdym wejsciu - samonaprawa.
+if (typeof navigator !== "undefined" && "serviceWorker" in navigator) {
+  navigator.serviceWorker.getRegistrations()
+    .then((regs) => regs.forEach((r) => r.unregister()))
+    .catch(() => { /* ignore */ });
+  if (typeof caches !== "undefined" && caches.keys) {
+    caches.keys().then((keys) => keys.forEach((k) => caches.delete(k))).catch(() => { /* ignore */ });
+  }
+}
+
 // Panel operacyjny (admin.trasa.travel) - osobny web-only entry.
 // BrowserRouter (nie HashRouter): admin jest tylko web na czystej domenie z
 // SPA-rewrite Vercela, wiec czyste URL-e (/moderacja, /users) bez '#'.
